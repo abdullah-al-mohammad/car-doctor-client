eed1b3d92deb3069f88b86170bf2b9ac52825d08
abdullah-al-mohammad <abdullahalmuhammad6@gmail.com>
2025-01-03 20:01:30 +0600

    add logout

--- src/pages/shaired/Navbar/Navbar.jsx
+++ src/pages/shaired/Navbar/Navbar.jsx
@@ -4,12 +4,21 @@ import logo from '../../../assets/logo.svg'
 import { AuthContext } from '../../../Providers/AuthProvider';
 
 const Navbar = () => {
-   const { user } = useContext(AuthContext)
+   const { user, logOut } = useContext(AuthContext)
+
+   const handleLogout = () => {
+      logOut()
+         .then(Result => { })
+         .then(error => {
+            console.error(error);
+
+         })
+   }
    const navItems = <>
       <li><Link to='/'>Home</Link></li>
       <li><Link to='/about'>About</Link></li>
-      {user ? <li><Link to='/'>Logout</Link></li>
-         : <li><Link to='/login'>Login</Link></li>}
+      {user?.email ? <li><button onClick={handleLogout}>Logout</button></li>
+         : <li><Link to='/signin'>Login</Link></li>}
    </>
    return (
       <div className="navbar bg-base-100 h-28 mb-3">
