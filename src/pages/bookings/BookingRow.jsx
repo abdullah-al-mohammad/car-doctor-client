10823712d9ce4e7ab465951e3dc5bbb1960b2900
abdullah-al-muhammad <abdullahalmuhammad6@gmail.com>
2025-01-05 12:39:03 +0600

    nothing

--- src/pages/bookings/BookingRow.jsx
+++ src/pages/bookings/BookingRow.jsx
@@ -1,43 +1,40 @@
 import React from "react";
 
-export const BookingRow = ({ booking, handleDelete, handleBookingConfirm }) => {
-	const { _id, services, date, price, img, status } = booking
-
+export const BookingRow = ({ booking }) => {
 	return (
 		<tr>
-			<button onClick={() => handleDelete(_id)} className="btn btn-sm btn-circle">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					className="h-6 w-6"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke="currentColor">
-					<path
-						strokeLinecap="round"
-						strokeLinejoin="round"
-						strokeWidth="2"
-						d="M6 18L18 6M6 6l12 12" />
-				</svg>
-			</button>
+			<th>
+				<label>
+					<input type="checkbox" className="checkbox" />
+				</label>
+			</th>
 			<td>
-				<div className="avatar">
-					<div className="rounded h-24 w-24">
-						{img && <img
-							src={img}
-							alt="Avatar Tailwind CSS Component"
-						/>}
+				<div className="flex items-center gap-3">
+					<div className="avatar">
+						<div className="mask mask-squircle h-12 w-12">
+							<img
+								src="https://img.daisyui.com/images/profile/demo/2@94.webp"
+								alt="Avatar Tailwind CSS Component"
+							/>
+						</div>
+					</div>
+					<div>
+						<div className="font-bold">Hart Hagerty</div>
+						<div className="text-sm opacity-50">United States</div>
 					</div>
 				</div>
 			</td>
 			<td>
-				{services}
+				Zemlak, Daniel and Leannon
+				<br />
+				<span className="badge badge-ghost badge-sm">
+					Desktop Support Technician
+				</span>
 			</td>
-			<td>{date}</td>
-			<td>${price}</td>
+			<td>Purple</td>
 			<th>
-				{status === 'confirm' ? <span className="font-bold text-primary">Confirmed</span> :
-					< button onClick={() => handleBookingConfirm(_id)} className="btn btn-ghost btn-xs">Please Confirm</button>}
+				<button className="btn btn-ghost btn-xs">details</button>
 			</th>
-		</tr >
+		</tr>
 	);
 };
