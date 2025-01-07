cf0922ea85bc80b3719eb50948b84839a3b2711c
abdullah-al-mohammad <abdullahalmuhammad6@gmail.com>
2025-01-07 10:07:37 +0600

    add cookie for secure data

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -21,10 +21,16 @@ const Login = () => {
                 const loggedInUser = result.user
                 // navigate(location?.state ? location?.state : '/')
                 const user = { email }
+
                 // get access token
-                axios.post('http://localhost:3000/jwt', user)
+                axios.post('http://localhost:3000/jwt', user, {
+                    withCredentials: true
+                })
                     .then(res => {
                         console.log(res.data);
+                        if (res.data.success) {
+                            navigate(location?.state ? location?.state : '/')
+                        }
 
                     })
             })
